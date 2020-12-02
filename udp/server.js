3b13625f3be860077963f51f4d2f28a26d55bc08
Ritwik Mukherjee <mritwik369@gmail.com>
2020-12-02 16:48:09 +0530

    datagram test

--- udp/server.js
+++ udp/server.js
@@ -7,3 +7,38 @@ server.on('error', function (error) {
     server.close();
 });
 
+server.on('message', function (msg, info) {
+    console.log('Data received from client : ' + msg.toString());
+    console.log('Received %d bytes from %s:%d\n', msg.length, info.address, info.port);
+
+    //sending msg
+    server.send(msg, info.port, 'localhost', function (error) {
+        if (error) {
+            client.close();
+        } else {
+            console.log('Data sent !!!');
+        }
+
+    });
+
+});
+
+server.on('listening', function () {
+    var address = server.address();
+    var port = address.port;
+    var family = address.family;
+    var ipaddr = address.address;
+    console.log('Server is listening at port' + port);
+    console.log('Server ip :' + ipaddr);
+    console.log('Server is IP4/IP6 : ' + family);
+});
+
+server.on('close', function () {
+    console.log('Socket is closed !');
+});
+
+server.bind(2222);
+
+// setTimeout(function () {
+//     server.close();
+// }, 8000);
